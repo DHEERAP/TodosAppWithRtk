ee7ebf1f28edd459a9b6f4ad01aaa75e86b3d67f
Dheerap Singh Tanwar <dheerapsinghtanwar360@gmail.com>
2024-10-01 10:22:18 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Todos from './components/Todos'
 function App() {
 return (
 <>
+<h1 className="text-3xl font-bold text-center text-black-600 mb-4 ">Write your daily Todos!</h1>
 <AddTodo />
 <Todos />
 </>
